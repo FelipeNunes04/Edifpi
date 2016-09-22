1224d797be08be6b10deee5a0fdb3cd39a11af63
felipenunes04 <felipennunes@hotmail.com>
2016-09-21 22:21:50 -0300

    Novas alterações

--- js/style.js
+++ js/style.js
@@ -1,6 +1,6 @@
  $(document).ready(function () {
    if ((window.screen.availWidth < 479))
-       document.getElementById("img").src = "images/slider/bg1_mobile.png";
+       document.getElementById("img").src = "images/topo/logo_mobile.png";
    else
-       document.getElementById("img").src = "images/slider/bg1.png";
+       document.getElementById("img").src = "images/topo/logo.png";
 })
